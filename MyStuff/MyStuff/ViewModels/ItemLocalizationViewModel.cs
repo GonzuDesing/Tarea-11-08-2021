1061336f5e6e383305962064297313ef23b8b456
GonzuDesing <gonzu.desing@gmail.com>
2021-08-11 19:36:42 -0600

    Add project files.

---
+++ MyStuff/MyStuff/ViewModels/ItemLocalizationViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MyStuff.Models;
+namespace MyStuff.ViewModels
+{
+    public class ItemLocalizationViewModel : BaseViewModel
+    {
+
+        public ItemLocalization MyLocation { get; set; }
+
+        //contructor
+        public ItemLocalizationViewModel()
+        {
+            MyLocation = new ItemLocalization();
+        }
+
+
+        public async Task<bool> GuardarLugar(string PLocated)
+        {
+            if (IsBusy) return false;
+
+            IsBusy = true;
+
+            try
+            {
+
+                MyLocation.Localization = PLocated;
+ 
+                bool R = await MyLocation.GuardarLocated();
+
+                return R;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+        }
+    }
+}
---
+++ MyStuff/MyStuff/Views/IngresoPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using MyStuff.ViewModels;
+
+namespace MyStuff.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IngresoPage : ContentPage
+    {
+
+        UserViewModel Vm;
+
+        public IngresoPage()
+        {
+            InitializeComponent();
+
+            BindingContext = Vm = new UserViewModel();
+
+        }
+
+        private void CmdVerPassword(object sender, ToggledEventArgs e)
+        {
+            if (SwVerPassword.IsToggled)
+            {
+                TxtPassword.IsPassword = false;
+            }
+            else
+            {
+                TxtPassword.IsPassword = true;
+            }
+        }
+
+        private async void CmdIngresar(object sender, EventArgs e)
+        {
+
+            bool R = await Vm.ValidarUsuario(TxtUsuario.Text.Trim(), TxtPassword.Text.Trim());
+
+            if (R)
+            {
+                await DisplayAlert(":)", "Usuario Correcto", "OK");
+
+                await Navigation.PushAsync(new SelectorDeAccionPage());
+
+            }
+            else
+            {
+                await DisplayAlert(":(", "Usuario Incorrecto", "OK");
+            }
+
+
+        }
+
+        private async void CmdRegistro(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistroPage());
+        }
+    }
+}
